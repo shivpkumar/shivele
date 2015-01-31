84d824d1fec6c95a9753f5b605e4bb090147e5eb
Shiv Kumar <shiv@newrelic.com>
2015-01-31 01:14:54 -0800

    Configure app for ActiveRecord

--- app.rb
+++ app.rb
@@ -1,3 +1,17 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+
+configure :development do
+  require 'pry'
+end
+
+configure :production do
+  require 'newrelic_rpm'
+end
+
+set :protection, except: :frame_options
+
 get '/' do
   send_file 'views/index.html'
 end
+
--- config.ru
+++ config.ru
@@ -1,14 +1,3 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'sinatra'
 require './app'
-
-configure :production do
-  require 'newrelic_rpm'
-end
-
-set :protection, except: :frame_options
-
 use Rack::Deflater
-
 run Sinatra::Application
